6f5881d77ca899884399a33959b0508f6d016ce5
John Lopez <john.lopez3695@gmail.com>
2024-12-26 15:45:20 -0500

    create task logic

--- src/services/tasks.ts
+++ src/services/tasks.ts
@@ -6,3 +6,15 @@ const prisma = new PrismaClient();
 export const getTasks = async (): Promise<Task[]> => {
   return await prisma.task.findMany();
 };
+
+export const createTask = async (data: CreateTaskDto): Promise<Task> => {
+  const task = await prisma.task.create({
+    data: {
+      title: data.title,
+      color: data.color,
+      completed: data.completed,
+    },
+  });
+  return task;
+};
+
