294a9ee524b5cc53f81c3b0cb88dea2e03a6fec9
John Lopez <john.lopez3695@gmail.com>
2024-12-26 15:46:30 -0500

    endpoint to delete a task

--- src/routes/tasks.ts
+++ src/routes/tasks.ts
@@ -6,4 +6,6 @@ const router = express.Router();
 router.get("/", taskController.getAllTasks);
 router.post("/", taskController.createTask);
 router.put("/:id", taskController.updateTask);
+router.delete("/remove/:id", taskController.deleteTask);
+
 export default router;
